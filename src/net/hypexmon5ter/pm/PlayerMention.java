b60abe8650741dd8dd08f04c8362c148f090df0c
HYPExMon5ter <BrackettBlake@gmail.com>
2017-07-18 21:40:04 -0400

    sends message onEnable

--- src/net/hypexmon5ter/pm/PlayerMention.java
+++ src/net/hypexmon5ter/pm/PlayerMention.java
@@ -1,30 +1,41 @@
 package net.hypexmon5ter.pm;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 public class PlayerMention extends JavaPlugin implements Listener {
 
-    public Set<UUID> exluded = new HashSet<>();
-    public Set<Player> cooldown = new HashSet<>();
+    public Set<UUID> excluded = new HashSet<>();
+    public ArrayList<Player> cooldown = new ArrayList<>();
 
+    public String consolePrefix = "[PlayerMention] ";
+    public String prefix = getConfig().getString("prefix");
     public boolean useOldWay = getConfig().getBoolean("useOldWay");
     public boolean needsPrefix = getConfig().getBoolean("needsPrefix");
-    public String prefix = getConfig().getString("Regular.prefix");
+    public String regPrefix = getConfig().getString("Regular.prefix");
+
+    ConsoleCommandSender console = Bukkit.getConsoleSender();
 
     public void onEnable() {
         getConfig().options().copyDefaults(true);
         saveConfig();
 
+        console.sendMessage("§8-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        console.sendMessage(consolePrefix + "§ahas successfully loaded, enjoy!");
+        console.sendMessage("§8-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
         getServer().getPluginManager().registerEvents(new MentionListener(this), this);
     }
 
     public void onDisable() {
-        cooldown.clear();
+
     }
 }
