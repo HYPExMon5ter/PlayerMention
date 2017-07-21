393f347db9045c46817c54cc72a6d5da065a9383
HYPExMon5ter <BrackettBlake@gmail.com>
2017-07-20 22:52:45 -0400

    Delete Placeholders.java

--- src/placeholderAPI/Placeholders.java
+++
@@ -1,9 +0,0 @@
-package placeholderAPI;
-
-import org.bukkit.entity.Player;
-
-public abstract interface Placeholders {
-    public abstract String setPlaceholders(Player paramPlayer, String paramString);
-
-    public abstract String pluginHooked();
-}
