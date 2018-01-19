33f5206574f21a13870b1c0a0a3db78b9fc8babf
HYPExMon5ter <BrackettBlake@gmail.com>
2018-01-18 23:48:08 -0500

    Fixed where player's we're still being mentioned when ignored via essentials (also fixes people mentioning someone that ignores them).

--- src/methods/MentionPlayer.java
+++ src/methods/MentionPlayer.java
@@ -31,60 +31,64 @@ public class MentionPlayer {
                     if (!(mentioner.hasPermission("pm.use"))) {
                         return;
                     }
-                } else {
-                    OnMentionEvent event = new OnMentionEvent(mentioner, target);
-                    Bukkit.getPluginManager().callEvent(event);
-                    if (!(event.isCancelled())) {
-                        if (PM.regMessageEnabled) {
-                            target.sendMessage(PM.convertPlaceholders(mentioner, PM.regMessage.replaceAll("%player%", mentioner.getName()).replaceAll("%nick%", mentioner.getDisplayName())));
+                }
+                if (PM.essentialsHook) {
+                    if (PM.ess.getUser(mentioner).isIgnoredPlayer(PM.ess.getUser(target)) || PM.ess.getUser(target).isIgnoredPlayer(PM.ess.getUser(mentioner))) {
+                        return;
+                    }
+                }
+                OnMentionEvent event = new OnMentionEvent(mentioner, target);
+                Bukkit.getPluginManager().callEvent(event);
+                if (!(event.isCancelled())) {
+                    if (PM.regMessageEnabled) {
+                        target.sendMessage(PM.convertPlaceholders(mentioner, PM.regMessage.replaceAll("%player%", mentioner.getName()).replaceAll("%nick%", mentioner.getDisplayName())));
+                    }
+
+                    try {
+                        if (EnumUtils.isValidEnum(Sound.class, PM.regSound)) {
+                            target.playSound(target.getLocation(), Sound.valueOf(PM.regSound), 100.0F, 1.0F);
+                        } else {
+                            target.playSound(target.getLocation(), Sounds.valueOf(PM.regSound).bukkitSound(), 100.0F, 1.0F);
                         }
+                    } catch (Exception e) {
+                        System.err.println(PM.prefix + ChatColor.RED + "That is not a valid sound (Error at 'Regular.sound')");
+                    }
 
-                        try {
-                            if (EnumUtils.isValidEnum(Sound.class, PM.regSound)) {
-                                target.playSound(target.getLocation(), Sound.valueOf(PM.regSound), 100.0F, 1.0F);
-                            } else {
-                                target.playSound(target.getLocation(), Sounds.valueOf(PM.regSound).bukkitSound(), 100.0F, 1.0F);
-                            }
-                        } catch (Exception e) {
-                            System.err.println(PM.prefix + ChatColor.RED + "That is not a valid sound (Error at 'Regular.sound')");
-                        }
+                    if (PM.regTitlesEnabled) {
+                        TitleAPI.sendTitle(target, 0, 5 * 20, 20, PM.convertPlaceholders(mentioner, PM.regTitle.replaceAll("%player%", mentioner.getName()).replaceAll("%nick%", mentioner.getDisplayName())), PM.convertPlaceholders(mentioner, PM.regSubtitle.replaceAll("%player%", mentioner.getName()).replaceAll("%nick%", mentioner.getDisplayName())));
+                    }
 
-                        if (PM.regTitlesEnabled) {
-                            TitleAPI.sendTitle(target, 0, 5 * 20, 20, PM.convertPlaceholders(mentioner, PM.regTitle.replaceAll("%player%", mentioner.getName()).replaceAll("%nick%", mentioner.getDisplayName())), PM.convertPlaceholders(mentioner, PM.regSubtitle.replaceAll("%player%", mentioner.getName()).replaceAll("%nick%", mentioner.getDisplayName())));
-                        }
+                    if (PM.regActionbarEnabled) {
+                        ActionbarAPI.sendActionBar(target, PM.convertPlaceholders(mentioner, PM.regActionbar.replaceAll("%player%", mentioner.getName()).replaceAll("%nick%", mentioner.getDisplayName())));
+                    }
+                    if (PM.regParticlesEnabled) {
+                        new BukkitRunnable() {
+                            double phi = 0;
 
-                        if (PM.regActionbarEnabled) {
-                            ActionbarAPI.sendActionBar(target, PM.convertPlaceholders(mentioner, PM.regActionbar.replaceAll("%player%", mentioner.getName()).replaceAll("%nick%", mentioner.getDisplayName())));
-                        }
-                        if (PM.regParticlesEnabled) {
-                            new BukkitRunnable() {
-                                double phi = 0;
+                            public void run() {
+                                Location loc = target.getLocation();
 
-                                public void run() {
-                                    Location loc = target.getLocation();
-
-                                    phi += Math.PI / 10;
-                                    for (double theta = 0; theta <= 2 * Math.PI; theta += Math.PI / 60) {
-                                        double r = 1.5;
-                                        double x = r * Math.cos(theta) * Math.sin(phi);
-                                        double y = r * Math.cos(phi) + 1.5;
-                                        double z = r * Math.sin(theta) * Math.sin(phi);
-                                        loc.add(x, y, z);
-                                        target.spawnParticle(Particle.valueOf(PM.regParticle), loc, 0, 0, 0, 0, 1);
-                                        loc.subtract(x, y, z);
-                                    }
-
-                                    if (phi > 3 * Math.PI) {
-                                        this.cancel();
-                                    }
-
+                                phi += Math.PI / 10;
+                                for (double theta = 0; theta <= 2 * Math.PI; theta += Math.PI / 60) {
+                                    double r = 1.5;
+                                    double x = r * Math.cos(theta) * Math.sin(phi);
+                                    double y = r * Math.cos(phi) + 1.5;
+                                    double z = r * Math.sin(theta) * Math.sin(phi);
+                                    loc.add(x, y, z);
+                                    target.spawnParticle(Particle.valueOf(PM.regParticle), loc, 0, 0, 0, 0, 1);
+                                    loc.subtract(x, y, z);
                                 }
-                            }.runTaskTimer(PM, 0, 1);
-                        }
 
-                        if (!(mentioner.hasPermission("pm.bypass") || mentioner.hasPermission("pm.admin"))) {
-                            MU.addToCooldown(mentioner);
-                        }
+                                if (phi > 3 * Math.PI) {
+                                    this.cancel();
+                                }
+
+                            }
+                        }.runTaskTimer(PM, 0, 1);
+                    }
+
+                    if (!(mentioner.hasPermission("pm.bypass") || mentioner.hasPermission("pm.admin"))) {
+                        MU.addToCooldown(mentioner);
                     }
                 }
             }
